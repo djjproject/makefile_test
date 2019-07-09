70354cf77dbbbecbf879af34ebf961579ca2d63f
djjproject <djj9405@naver.com>
2019-07-09 16:15:08 +0900

    cmake

---
+++ CMakeFiles/diary.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/diary.dir/main.c.o"
+  "CMakeFiles/diary.dir/calendar.c.o"
+  "CMakeFiles/diary.dir/memo.c.o"
+  "diary.pdb"
+  "diary"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/diary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
